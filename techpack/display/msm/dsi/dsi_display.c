74e1891481f1d187bda60d76a4b20431f5fca5db
NotZeetaa <rodrigo2005contente@gmail.com>
2022-11-05 10:19:17 +0000

    Revert "techpack: display: Add config to enable/disable OSS FOD support"
    This reverts commit a290fe8fcf8a6012370105949701c06da926674c.
    
    * No miui support

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -53,9 +53,7 @@ static const struct of_device_id dsi_display_dt_match[] = {
 	{}
 };
 
-#ifdef CONFIG_OSSFOD
 struct dsi_display *primary_display;
-#endif
 
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
@@ -5283,7 +5281,6 @@ static struct attribute_group dynamic_dsi_clock_fs_attrs_group = {
 	.attrs = dynamic_dsi_clock_fs_attrs,
 };
 
-#ifdef CONFIG_OSSFOD
 static ssize_t sysfs_fod_ui_read(struct device *dev,
 	struct device_attribute *attr, char *buf)
 {
@@ -5373,7 +5370,6 @@ static struct attribute *display_fs_attrs[] = {
 static struct attribute_group display_fs_attrs_group = {
 	.attrs = display_fs_attrs,
 };
-#endif
 
 static int dsi_display_sysfs_init(struct dsi_display *display)
 {
@@ -5384,11 +5380,9 @@ static int dsi_display_sysfs_init(struct dsi_display *display)
 		rc = sysfs_create_group(&dev->kobj,
 			&dynamic_dsi_clock_fs_attrs_group);
 
-#ifdef CONFIG_OSSFOD
 	rc = sysfs_create_group(&dev->kobj, &display_fs_attrs_group);
 	if (rc)
 		pr_err("failed to create display device attributes");
-#endif
 
 	return rc;
 
@@ -5402,23 +5396,19 @@ static int dsi_display_sysfs_deinit(struct dsi_display *display)
 		sysfs_remove_group(&dev->kobj,
 			&dynamic_dsi_clock_fs_attrs_group);
 
-#ifdef CONFIG_OSSFOD
 	sysfs_remove_group(&dev->kobj,
 		&display_fs_attrs_group);
-#endif
 
 	return 0;
 
 }
 
-#ifdef CONFIG_OSSFOD
 void dsi_display_set_fod_ui(struct dsi_display *display, bool status)
 {
 	struct device *dev = &display->pdev->dev;
 	atomic_set(&display->fod_ui, status);
 	sysfs_notify(&dev->kobj, NULL, "fod_ui");
 }
-#endif
 
 /**
  * dsi_display_bind - bind dsi device with controlling device
@@ -5493,9 +5483,7 @@ static int dsi_display_bind(struct device *dev,
 		goto error;
 	}
 
-#ifdef CONFIG_OSSFOD
 	atomic_set(&display->fod_ui, false);
-#endif
 
 	memset(&info, 0x0, sizeof(info));
 
@@ -6857,9 +6845,7 @@ int dsi_display_get_modes(struct dsi_display *display,
 exit:
 	*out_modes = display->modes;
 	rc = 0;
-#ifdef CONFIG_OSSFOD
 	primary_display = display;
-#endif
 
 error:
 	if (rc)
@@ -8442,12 +8428,9 @@ int dsi_display_unprepare(struct dsi_display *display)
 	return rc;
 }
 
-#ifdef CONFIG_OSSFOD
-struct dsi_display *get_main_display(void)
-{
+struct dsi_display *get_main_display(void) {
 	return primary_display;
 }
-#endif
 
 static int __init dsi_display_register(void)
 {
--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -177,13 +177,11 @@ struct drm_panel_esd_config {
 	u32 groups;
 };
 
-#ifdef CONFIG_OSSFOD
 #define BRIGHTNESS_ALPHA_PAIR_LEN 2
 struct brightness_alpha_pair {
 	u32 brightness;
 	u32 alpha;
 };
-#endif
 
 struct dsi_panel {
 	const char *name;
@@ -243,10 +241,8 @@ struct dsi_panel {
 	
 	int hbm_mode;
 
-#ifdef CONFIG_OSSFOD
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
-#endif
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -383,11 +379,9 @@ int dsi_panel_create_cmd_packets(const char *data,
 void dsi_panel_destroy_cmd_packets(struct dsi_panel_cmd_set *set);
 void dsi_panel_dealloc_cmd_packets(struct dsi_panel_cmd_set *set);
 
-#ifdef CONFIG_OSSFOD
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 
 u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
-#endif
 
 int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
 
